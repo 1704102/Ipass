b63296083cd8728320df3a17434fe8c14f8dc25c
DESKTOP-AS6O349\marti <martijn.m.bakker@student.hu.nl>
2017-05-04 16:29:08 +0200

    - implemented Database methods

--- src/Application/Database/ReserveringDatabase.java
+++ src/Application/Database/ReserveringDatabase.java
@@ -6,9 +6,9 @@ package Application.Database;
 public class ReserveringDatabase extends DatabaseHelper{
 
     public void inserReservering(String name, String surName, String email, String personen, String date, String time){
-        String date1 = "STR_TO_DATE('" + date + " " + time + "','%m-%d-%Y %H:%i:%s')";
+        String date1 = "STR_TO_DATE('" + date + " " + time + ":00','%Y-%m-%d %H:%i:%s')";
         System.out.println(date1);
-//        insert(String.format("insert into Reserveringen (voornaam, achternaam, email, personen, datum) values (\"%s\",\"%s\",\"%s\",\"%d\",\"%s\")",
-//                name, surName, email, Integer.parseInt(personen), date1));
+        insert(String.format("insert into Reserveringen (voornaam, achternaam, email, personen, datum) values (\"%s\",\"%s\",\"%s\",%d,%s)",
+                name, surName, email, Integer.parseInt(personen), date1));
     }
 }
